49df5c388fdf48d66025f56a472c50cfd24b64e6
gongvirgil <gongvirgil@gmail.com>
2017-03-27 19:11:29 +0800

    add more thx

--- Application/Cli/Controller/TestController.class.php
+++ Application/Cli/Controller/TestController.class.php
@@ -17,57 +17,42 @@ class TestController extends Controller {
         $res = $Cmd->execute($cmdLine);
         var_dump($res);
     }
-    public function aaaa(){
-        $pro = M('area_dict')->where('level=2')->order('id asc')->select();
-        foreach ($pro as $k => $v) {
-            $a = explode("市", $v['name']);
-            $b = explode("地区", $v['name']);
-            $c = explode("自治州", $v['name']);
-            if(count($a)==2){
-                M('area_dict')->where('id='.$v['id'])->save(array('level_type'=>"市"));
-            }elseif(count($b)==2){
-                M('area_dict')->where('id='.$v['id'])->save(array('level_type'=>"地区"));
-            }elseif(count($c)==2){
-                M('area_dict')->where('id='.$v['id'])->save(array('level_type'=>"自治州"));
-            }else{
-                var_dump($v);
-               //M('area_dict')->where('id='.$v['id'])->save(array('level_type'=>"直辖市")); 
-            }
-        }
-    }
-    public function aaa(){
-        $city = M('area_dict')->where('level=3')->order('id asc')->select();
-        foreach ($city as $k => $v) {
-            $pro = M('area_dict')->where('level=2 and id='.$v['pid'])->find();
-            $map['merger_short_name'] = $pro['merger_short_name'].",".$v['short_name'];
-            M('area_dict')->where('id='.$v['id'])->save($map);
-        }   
-    }
 
-    public function aa(){
-        $pro = M('area_dict')->where('level=2')->select();
-        foreach ($pro as $k => $v) {
-            $map['id'] = $v['id'];
-            $id = M()->table('sh_area')->where("level=2 and name='".$v['name']."'")->getField('id');
-            $city = M()->table('sh_area')->where('level=3 and pid='.$id)->select();
-            foreach ($city as $k1 => $v1) {
-                $map['id'] = $map['id']+1;
-                $map['pid']= $v['id'];
-                $map['level'] = 3;
-                $map['name'] = $v1['name'];
-                $map['merger_name'] = $v1['merger_name'];
-                $map['short_name'] = $v1['shortname'];
-                $map['merger_short_name'] = $v1[''];
-                $map['en_name'] = $v1['pinyin'];
-                $map['initial'] = $v1['first'];
-                $map['code'] = $v1['code'];
-                $map['zipcode'] = $v1['zip_code'];
-                $map['longitude'] = $v1['lng'];
-                $map['latitude'] = $v1['lat'];
-                $res = M('area_dict')->add($map);
-                if(!$res) var_dump(M()->_sql());
+
+    public function aa(){     
+        $i=0;
+        for($area = 0x00; $area <= 0xff; $area++){
+          for($pos = 0x00; $pos <= 0xff; $pos++){
+            $w = pack('CC', $area, $pos);
+
+            $gb2312_name = iconv('GB2312', 'UTF-8', $w);
+            $gbk_name = iconv('GBK', 'UTF-8', $w);
+            $gb18030_name = iconv('GB18030', 'UTF-8', $w);
+            $map['id'] = $i;
+            $map['code'] = strtoupper(dechex($area).dechex($pos));
+
+            if(strlen($gb2312_name)==3) $map['in_gb2312'] = 1;
+            if(strlen($gbk_name)==3) $map['in_gbk'] = 1;
+            if(strlen($gb18030_name)==3) $map['in_gb18030'] = 1;
+            
+            if(strlen($gb2312_name)==3) {
+                $map['name'] = $gb2312_name;
+            }elseif(strlen($gbk_name)==3){
+                $map['name'] = $gbk_name;
+            }elseif(strlen($gb18030_name)==3){
+                $map['name'] = $gb18030_name;
+            }else{
+                $map['name'] = "";
             }
+            $res = M('chinese_character')->add($map);
+            $i++;
+          }      
         }
-        
+        var_dump($i);
+    }
+    public function ab(){
+        $w = pack('CC', 0xa9, 0x96);
+        $a= iconv('GB2312', 'UTF-8', $w);
+        if(!empty($a)) var_dump($a);
     }
 }
