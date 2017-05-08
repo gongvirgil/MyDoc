5a5ad6a3620614dfe705bd9e9db6d2e1cfe2f51e
gongvirgil <gongvirgil@gmail.com>
2017-05-08 19:06:29 +0800

    change sth

--- Application/Api/Controller/TestController.class.php
+++ Application/Api/Controller/TestController.class.php
@@ -9,7 +9,7 @@ class TestController extends Controller {
 		for ($i=0; $i < 10; $i++) {
 			$arr[$i] = $Random->letter();
 		}
-		$Redis->set('numbers',$arr);
+		$Redis->set('numbers','1bcd');
 		$numbers = $Redis->get('numbers');
 		var_dump($numbers);
 	}
@@ -17,5 +17,11 @@ class TestController extends Controller {
 		$Redis = \Lib\Virgil\Redis::init();
 		$numbers = $Redis->get('numbers');
 		var_dump($numbers);
+	}
+	public function cc(){
+		$Redis = \Lib\Virgil\Redis::init();
+		$numbers = $Redis->keys();
+		var_dump($numbers);
+		
 	}
 }
--- Lib/Virgil/Redis.class.php
+++ Lib/Virgil/Redis.class.php
@@ -4,18 +4,18 @@ class Redis {
     public static $instance;
     public $redis;
 
-    private function __construct() {}
-    private function __clone() {}
+    private function __construct(){}
+    private function __clone(){}
 	/**
 	 * [init 初始化Redis]
 	 * @param  array  $config [description]
 	 * @return [type]         [description]
 	 */
-    public static function init($config=array()) { 
-        if (null === self::$instance) {
+    public static function init($config=array()){ 
+        if (null === self::$instance){
             self::$instance = new self();
             self::$instance->redis = new \Redis();
-	        if(empty($config['server']))  $config['server'] = '127.0.0.1';  
+	        if(empty($config['server']))  $config['server'] = '127.0.0.1';
 	        if(empty($config['port']))  $config['port'] = '6379';
             self::$instance->redis->connect($config['server'], $config['port']); 
         }
@@ -25,78 +25,78 @@ class Redis {
      * [redis 获取redis对象]
      * @return [type] [description]
      */
-    public function redis() {  
-        return $this->redis;  
-    }  
+    public function redis(){
+        return $this->redis;
+    }
     /**
      * [flushAll 清空数据]
      * @return [type] [description]
      */
-    public function flushAll() {  
-        return $this->redis->flushAll();  
-    }  
+    public function flushAll(){
+        return $this->redis->flushAll();
+    }
     /**
      * [set 设置值]
      * @param [type]  $key     [description]
      * @param [type]  $value   [description]
      * @param integer $timeOut [生命时间]
      */
-    public function set($key, $value, $timeOut=0) {  
-        $value = json_encode($value, true);  
-        $result = $this->redis->set($key, $value);  
-        if($timeOut > 0) $this->redis->setTimeout($key, $timeOut);  
+    public function set($key, $value, $timeOut=0){
+        $value = json_encode($value);
+        $result = $this->redis->set($key, $value);
+        if($timeOut > 0) $this->redis->setTimeout($key, $timeOut);
         return $result;
-    }  
+    }
     /**
      * [setTimeout 设置生命时间]
      * @param [type] $key     [description]
      * @param [type] $timeOut [description]
      */
     public function setTimeout($key, $timeOut){
-        return $this->redis->setTimeout($key, $timeOut);  
+        return $this->redis->setTimeout($key, $timeOut);
     }
     /**
      * [get 获取值]
      * @param  [type] $key [description]
      * @return [type]      [description]
      */
-    public function get($key) {  
+    public function get($key){
         $result = $this->redis->get($key);
         $value = json_decode($result, true);
         return $value;
-    }  
+    }
     /**
      * [delete 删除一条数据]
      * @param  [type] $key [description]
      * @return [type]      [description]
      */
-    public function delete($key) {  
-        return $this->redis->delete($key);  
+    public function delete($key){
+        return $this->redis->delete($key);
     } 
     /**
      * [exists 判断key是否存在]
      * @param  [type] $key [description]
      * @return [type]      [ture/false]
      */
-    public function exists($key) {  
-        return $this->redis->exists($key);  
-    }  
+    public function exists($key){
+        return $this->redis->exists($key);
+    }
     /**
      * [incr 自增]
      * @param  [type] $key [description]
      * @return [type]      [description]
      */
-    public function incr($key) {  
-        return $this->redis->incr($key);  
-    }  
+    public function incr($key){
+        return $this->redis->incr($key);
+    }
     /**
      * [decr 自减]
      * @param  [type] $key [description]
      * @return [type]      [description]
      */
-    public function decr($key) {  
-        return $this->redis->decr($key);  
-    }    
+    public function decr($key){
+        return $this->redis->decr($key);
+    }  
     /**
      * [push 入队列]
      * @param  [type]  $key   [description]
@@ -104,25 +104,25 @@ class Redis {
      * @param  boolean $r     [description]
      * @return [type]         [description]
      */
-    public function push($key, $value,$r=true) {  
-        $value = json_encode($value, true);  
-        return $r ? $this->redis->rPush($key, $value) : $this->redis->lPush($key, $value);  
-    }  
+    public function push($key, $value,$r=true){
+        $value = json_encode($value, true);
+        return $r ? $this->redis->rPush($key, $value) : $this->redis->lPush($key, $value);
+    }
     /**
      * [pop 出队列]
      * @param  [type]  $key [description]
      * @param  boolean $l   [description]
      * @return [type]       [description]
      */
-    public function pop($key, $l=true) {  
-        $val = $l ? $this->redis->lPop($key) : $this->redis->rPop($key);  
+    public function pop($key, $l=true){
+        $val = $l ? $this->redis->lPop($key) : $this->redis->rPop($key);
         return json_decode($val, true);
-    }  
+    }
     /**
      * [keys 获取所有key]
      * @return [type] [description]
      */
-    public function keys() {
+    public function keys(){
         return $this->redis->keys('*');
     }
    /**
